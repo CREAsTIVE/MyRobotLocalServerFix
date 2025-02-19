e8afcc38c31263d7099cc7fcf1cff3e62437304d
CREAsTIVE <alexshveev@gmail.com>
2025-02-20 01:00:48 +0500

    Добавьте файлы проекта.

---
+++ MyRobotLocalServerFix/MyRobotLocalServerFixMod.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using System;
+
+namespace MyRobotLocalServerFix
+{
+	[HarmonyLib.HarmonyPatch(typeof(Game), nameof(Game.SetClient))]
+	public class SetClientPatch
+	{
+		static void Prefix(ref MyRobotClient _session)
+		{
+			_session.oobaServerPath = "http://127.0.0.1:5000";
+		}
+	}
+
+	public class MyRobotLocalServerFixMod : MelonMod
+	{
+
+	}
+}
+											   
---
+++ MyRobotLocalServerFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using MelonLoader;
+using MyRobotLocalServerFix;
+using System.Runtime.InteropServices;
+
+[assembly: MelonInfo(typeof(MyRobotLocalServerFixMod), "MyRobot local server fix", "0.1", "CREAsTIVE")]
+// [assembly: MelonGame("Game Developer", "Game Name")]
